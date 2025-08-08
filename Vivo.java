1d45dd06ad93f44039fee977b8148ba7ce2ae738
prajwalb1607 <prajwalb.xworks@gmail.com>
2025-08-08 16:26:43 +0530

    java program

---
+++ Vivo.java
@@ -0,0 +1,17 @@
+class Vivo{
+      public static void main(String args[]){
+	  byte ramOfvivo=8;
+	  byte ramOfredme=6;
+	  byte ramOfappo=8;
+	  byte ramOfoneplus=10;
+
+	  System.out.println("ram size of the vivo phone is :"+ramOfvivo);
+	  
+	  System.out.println("ram size of the redme phone is: "+ramOfredme);
+	  
+	  System.out.println("ram size of the appo phone is :"+ramOfappo);
+	  
+	  System.out.println("ram size of the one plus phone is: "+ramOfoneplus);
+	  
+	 }
+}
