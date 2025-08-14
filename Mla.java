f1511a7a02c7b48c7ccc6e5d95d66ed0ffb9c7c2
prajwalb1607 <prajwalb.xworks@gmail.com>
2025-08-14 15:08:35 +0530

    Add files via upload

---
+++ Mla.java
@@ -0,0 +1,25 @@
+class Mla{
+      public static void main(String [] args){
+	  byte age=50;
+	  age=70;
+	  short legistativeAssembly=224;
+	  int price=12345;
+	  price=16783;
+	  long contactNumber=7090356782l;
+	  char gender='m';
+	  float numberOfvoters=1234.123456f;
+	  double numberOfvotes=1234.13134;
+	  boolean isAvailable=true;
+	  float numberOfmalevoters=12345.14151f;
+	  System.out.println("age of the mla is:"+age);
+	  System.out.println("legistativeAssembly of constituent assembly is:"+legistativeAssembly);
+	  System.out.println("price of mla is invested is :"+price);
+	  System.out.println("contactNumber of the mla is :"+contactNumber);
+	  System.out.println("gender of the mla is :"+gender);
+	  System.out.println("numberOfvoters in assembly is :"+numberOfvoters);
+	  System.out.println("numberOfvotes in assembly is :"+numberOfvotes);
+	  System.out.println("mla isAvailable in assembly:"+isAvailable);
+	  System.out.println("numberOfmalevoters in assembly:"+numberOfmalevoters);
+}
+}	  
+	  
