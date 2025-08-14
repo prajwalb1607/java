b68855639b18708761bb1a1c82002c2b5255bee0
prajwalb1607 <prajwalb.xworks@gmail.com>
2025-08-14 15:09:38 +0530

    Add files via upload

---
+++ Hyundai.java
@@ -0,0 +1,28 @@
+class Hyundai{
+      public static void main(String [] args){
+	  byte speed=80;
+	  speed=90;
+	  short engine=1482;
+	  engine=1367;
+	  int price=10035;
+	  long contactNumber=709099124;
+	  char model='z';
+	  float gst=1313.13135256f;
+	  double sgst=1234.1234789999;
+	  boolean isAvailable=true;
+	  System.out.println("speed of the hyundai car is :"+speed);
+	  if(speed==engine){
+		  
+	  System.out.println("yes both are in same location");
+	  }else{
+	  	  System.out.println("no both are in same location");
+      System.out.println("price of the hyundai car is :"+price);
+	  System.out.println("contactNumber of the show room is :"+contactNumber);
+	  System.out.println("model of the hyundai car is :"+model);
+	  System.out.println("gst of the hyundai car is :"+gst);
+	  System.out.println("sgst of the hyundai car is :"+sgst);
+	  System.out.println("hyundai car is  avaiable :"+isAvailable);
+	}
+}
+	  
+	  
