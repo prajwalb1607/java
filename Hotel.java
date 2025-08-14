7dd121cc4b01fe29c9692816df245535af26195e
prajwalb1607 <prajwalb.xworks@gmail.com>
2025-08-14 15:07:51 +0530

    Add files via upload

---
+++ Hotel.java
@@ -0,0 +1,20 @@
+class Hotel{
+      public static void main(String [] args){
+	  byte hotelNumber=56;
+	  short addressNumber=123;
+	  int price=55768;
+	  long contact=7353829222l;
+	  char series='a';
+	  float gst=123.124566f;
+	  double sgst=12445.141661727;
+	  boolean isAvailable=true;
+	  	  System.out.println("hotelNumber is :"+hotelNumber);
+	  System.out.println("addressNumber of the hotel is  :"+addressNumber);
+      System.out.println("price of food in hotel is:"+price);
+	  System.out.println("contact of hotel is  :"+contact);
+	  System.out.println("series ina hotel is :"+series);
+	  System.out.println("gst in hotel is  :"+gst);
+	  System.out.println("sgst in hotel is  :"+sgst);
+	  System.out.println("hotel  is avaiable in every time  :"+isAvailable);
+	}
+}
