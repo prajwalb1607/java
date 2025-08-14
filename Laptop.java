6ba8d382c5563ab126ba06561dc528f519ceae7f
prajwalb1607 <prajwalb.xworks@gmail.com>
2025-08-14 15:02:20 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,14 @@
+class Laptop{
+      public static void main(String [] args){
+	  String  []laptopComponents={"keyboard","battery","cpu","screen","ram"};
+	  String  [] smartPhoneComponents={"display","battery","processor","camera","motherBoard"};
+	  String  [] bugerIndegeridents={"bun","tomato","chess","beefPatty","lettuce"};
+	  String  [] bicycleComponents={"wheels","chain","brakes","pedals","frame"};
+	  String  [] pizzaIndegeridents={"dough","tomatoSauce","cheese","toppings","oil"};
+	  System.out.println("laptop components is :"+laptopComponents[2]);
+	  System.out.println("components of the smart phone is :"+smartPhoneComponents[1]);
+	  System.out.println("indegeridents of the buger is :"+bugerIndegeridents[4]);
+	  System.out.println("components of the bicycle is :"+bicycleComponents[3]);
+	  System.out.println("indegeridents of the pizza is :"+pizzaIndegeridents[2]);
+	 }
+}
