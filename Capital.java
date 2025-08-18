4a392e907e2b7e3ddaaa40f72fb50a356b2a64db
prajwalb1607 <prajwalb.xworks@gmail.com>
2025-08-18 14:46:32 +0530

    Add files via upload

---
+++ Capital.java
@@ -0,0 +1,26 @@
+class Capital{
+       public static void main(String args[]){
+	   String karanatakaCapital="bengaluru";
+	   String tamilnaduCapital="chennai";
+	   String andrapradeshCapital="vizag";
+	   String telaganaCapital="hyderabad";
+	   String maharasataraCapital="mumbai";
+	   String uttrapradeshCapital="lucknow";
+	   String biharCapital="patna";
+	   String goaCapital="panaji";
+	   String westbengalCapital="kolkata";
+	   String haryanaCapital="chandigarh";
+	   System.out.println(karanatakaCapital);
+	   System.out.println(tamilnaduCapital);
+	   System.out.println(andrapradeshCapital);
+	   System.out.println(telaganaCapital);
+	   System.out.println(maharasataraCapital);
+	   System.out.println(uttrapradeshCapital);
+	   System.out.println(biharCapital);
+	   System.out.println(goaCapital);
+	   System.out.println(westbengalCapital);
+	      System.out.println(haryanaCapital);
+	   
+	   
+	   }
+}
