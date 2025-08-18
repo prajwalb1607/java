86da59040cafdbf12ce7a83d5e221234603b10ce
prajwalb1607 <prajwalb.xworks@gmail.com>
2025-08-18 14:47:57 +0530

    Add files via upload

---
+++ Jio.java
@@ -0,0 +1,11 @@
+class Jio{
+       public static void main(String [] args){
+
+	   //for each
+	   	   String teluguMovies[]={"rrr","bahubali","pushpa","devara","ega"};
+		   int i;
+		   for(String movies:teluguMovies){
+			   System.out.println("teluguMovies are ;"+movies);
+	  }
+	 }
+}
