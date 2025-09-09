185701de1fd1e0a113cdea46d3d4462570462fe7
prajwalb1607 <prajwalb.xworks@gmail.com>
2025-09-09 12:26:21 +0530

    method

---
+++ Method1.java
@@ -0,0 +1,37 @@
+class Method1 {
+    public static byte getByteValue() {
+        return 20;
+    }
+
+    public static short getShortValue() {
+        return 1267;
+    }
+
+    public static int getIntValue() {
+        return 5000;
+    }
+
+    public static long getLongValue() {
+        return 123456789L;
+    }
+
+    public static char getCharValue() {
+        return 'A';
+    }
+
+    public static float getFloatValue() {
+        return 12.34f;
+    }
+
+    public static double getDoubleValue() {
+        return 56789.123;
+    }
+
+    public static boolean getBooleanValue() {
+        return true;
+    }
+	public static String getStringValue(){
+		return "sagar";
+	}
+	
+}
